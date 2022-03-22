eee1ab7b56e498414e0cc41ec3d4d50476a307a3
gabrielhuluany <gabrielhuluany@outlook.com>
2022-03-22 15:18:15 -0300

    Create all .cs files with Hello World

---
+++ Ex1/Ex1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program {
+  public static void Main (string[] args) {
+    Console.WriteLine ("Hello World");
+    
+    Console.WriteLine("Pressione qualquer tecla para fechar esta janela...");
+    Console.ReadKey();
+  }
+}
